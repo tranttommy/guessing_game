aba32079b7a0aafe4bb48e461c2f1d2d698a2c09
Tommy Tran <tommy@squishymedia.com>
2020-02-07 14:40:03 -0800

    main takes user input and prints it back

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    println!("Please input your guess.");
+    let mut guess = String::new();
+
+    std::io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read the line");
+
+    println!("You guessed: {}", guess);
 }
